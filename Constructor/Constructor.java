a9e858fc1ed5b91f38621c30bdd54ef9bc845ce6
Yogaprasanth17 <yogivelu17@gmai.com>
2023-07-30 12:50:00 +0530

    Constructor_Java

---
+++ Constructor/Constructor.java
@@ -0,0 +1,10 @@
+package Constructor;
+public class Constructor {
+    public Constructor()
+    {
+        System.out.println("Your in Construtor");
+    }
+    public static void main(String[] args){
+        Constructor constructor =new Constructor();
+    }
+}
