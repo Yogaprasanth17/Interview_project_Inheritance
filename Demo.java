d2052a480e1f185f8849ab60ec8ba4fe82600136
Yogaprasanth17 <yogivelu17@gmai.com>
2023-07-30 12:26:19 +0530

    My_First_commit

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public Demo()
+    {
+        System.out.print("Hello World");
+    }
+    public static void main(String[] args)
+    {
+        Demo demo = new Demo();
+    }
+}
