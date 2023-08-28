377bfe59cda243e77e2562e1bdc779fdac0bda23
Yogaprasanth17 <yogivelu17@gmai.com>
2023-08-29 00:07:42 +0530

    Second_pattern_29/08/2023

---
+++ Pattern/HalfTriangle.java
@@ -0,0 +1,34 @@
+package Pattern;
+
+/*
+ col   row
+  1      1    * 
+  2      2    * * 
+  3      3    * * * 
+  4      4    * * * *
+  5      5    * * * * *
+  4      6    * * * *
+  3      7    * * *
+  2      8    * *
+  1      9    *
+
+        here n = 2 * num - 1 
+ */
+
+public class HalfTriangle {
+    
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int row = 1; row <= 2 * num - 1; row++) {
+            
+            int colToRun = (row > num) ? (2 * num) - row : row;
+
+            for (int col = 1; col <= colToRun; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
