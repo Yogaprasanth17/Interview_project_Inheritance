8e6ed14b9a1bea3f04bf9be52d356e9336a61e9d
Yogaprasanth17 <yogivelu17@gmai.com>
2023-08-30 22:42:32 +0530

    Diamond_30/08/2023

---
+++ Pattern/HalfDiamond.java
@@ -0,0 +1,50 @@
+package Pattern;
+
+public class HalfDiamond {
+
+/*
+    
+ O/P
+
+            *
+          * * *
+        * * * * *
+      * * * * * * *
+    * * * * * * * * *
+
+ */
+
+    public static void main(String[] args) {
+        int num = 5;
+
+        for (int row = 1; row <= num; row++) {
+            int t = num - row; 
+            int sp = t  - row;
+            for (int space = 1; space <= t; space++) {
+                System.out.print("  ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
