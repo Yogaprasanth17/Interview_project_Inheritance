2f0620759bf2f3f49c4e776a21424cc6ea01a225
Yogaprasanth17 <yogivelu17@gmai.com>
2023-09-24 11:20:57 +0530

    Factorial_24/09/2023

---
+++ Recursion/Factorial.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class Factorial {
+    public static void main(String[] args){
+        int num = 5;
+        System.out.println(factorial(num)); 
+    }
+    public static int factorial(int num){
+        if(num == 1 || num == 0) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+}
