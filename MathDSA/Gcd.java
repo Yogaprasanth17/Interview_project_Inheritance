719e65a4ec0efb4490d285dfaa386c3666cd536b
Yogaprasanth17 <yogivelu17@gmai.com>
2023-10-09 16:18:52 +0530

    Gcd_and_Lcm_9/10/2023

---
+++ MathDSA/Gcd.java
@@ -0,0 +1,28 @@
+package MathDSA;
+
+// gcd using recursion  
+
+public class Gcd {
+    public static void main(String[] args) {
+        int x = 2, y = 7; // o/p = 14
+        System.out.print(gcdNum(x,y));
+        System.out.print(lcmNum(x,y));
+    }
+
+     static int lcmNum(int x, int y) {
+        // the least number which divided by both x and y
+        /*
+         * formula : LCM = a * b / gcd(a,b)
+         */
+        return (x * y) / gcdNum(x , y) ;
+    }
+
+    static int gcdNum(int a,int b) {
+        // here b % a and "a" is passed as a parameter because "b" is % by "a" so "a" will become 0 
+        if(a == 0) {
+            return b;
+        }
+        return gcdNum((b%a), a);
+    }
+
+}
